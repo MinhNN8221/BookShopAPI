fc1ec0243fa9ce2aa3bbc97e67087fdbd761e82b
MinhNN8221 <itptit2001@gmail.com>
2023-10-15 23:10:49 +0700

    update

--- src/main/java/com/example/bookshopapi/BookShopAPIApplication.java
+++ src/main/java/com/example/bookshopapi/BookShopAPIApplication.java
@@ -8,13 +8,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class BookShopAPIApplication {
 	public static void main(String[] args) {
-//		SpringApplication.run(BookShopAPiApplication.class, args);
-		ConfigurableApplicationContext context = SpringApplication.run(BookShopAPIApplication.class, args);
-
-		DatabaseConnectionTest connectionTest = context.getBean(DatabaseConnectionTest.class);
-		connectionTest.testDatabaseConnection();
-
-		// Đóng ứng dụng
-		context.close();
+		SpringApplication.run(BookShopAPIApplication.class, args);
 	}
 }
